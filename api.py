3e3328eaa3fbfe6b7ab18de1853fbe1b1127e033
eswaran s <eswares11111999@gmail.com>
2022-02-09 15:53:15 +0530

    doctor app

---
+++ api.py
@@ -0,0 +1,216 @@
+import pymysql
+from app import app
+from db import mysql
+from flask import Flask, jsonify
+from flask import flash, request
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/doctors')
+def doctors():
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM doctor_app.doctor;")
+        rows = cursor.fetchall()
+        resp = jsonify(rows)
+        resp.status_code=200
+        print(rows)
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close() 
+        conn.close()
+
+@app.route('/doctordetails')
+def doctors_details():
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM doctor_app.doctor;")
+        rows = cursor.fetchall()
+        resp = jsonify(rows)
+        resp.status_code=200
+        print(rows)
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close() 
+        conn.close()
+@app.route('/create-appointment', methods=['POST'])
+def create_appointment():
+    conn = None
+    cursor = None
+    try:
+        _json = request.json
+        _appointmentname = _json['appointmentname']
+        _appointmentdate = _json['appointmentdate']
+        _appointmentdoctorname = _json['appointmentdoctorname']
+        _appointmentemail = _json['appointmentemail']
+        _appointmenttext = _json['appointmenttext']
+        print("test")
+		# validate the received values
+        if _appointmentname and _appointmentdate and _appointmentdoctorname and _appointmentemail and _appointmenttext and request.method == 'POST':
+			
+			# save edits  
+             sql = "INSERT INTO appointment(appointmentname, appointmentdate, appointmentdoctorname, appointmentemail, appointmenttext) VALUES(%s, %s, %s, %s, %s)" 
+             data = (_appointmentname, _appointmentdate, _appointmentdoctorname, _appointmentemail, _appointmenttext)
+             conn = mysql.connect()
+             cursor = conn.cursor()
+             cursor.execute(sql, data)
+             conn.commit()
+             resp = jsonify('User added successfully!')
+             resp.status_code = 200
+             return resp
+        else:
+            return not_found()
+    except Exception as e:
+		     print(e)
+    finally:
+        cursor.close() 
+        conn.close()
+
+
+@app.route('/add', methods=['POST'])
+def add_user():
+    conn = None
+    cursor = None
+    try:
+        _json = request.json
+        _doctorname = _json['doctorname']
+        _doctornumber = _json['doctornumber']
+        _doctorappointment = _json['doctorappointment']
+        print("test")
+		# validate the received values
+        if _doctorname and _doctornumber and _doctorappointment and request.method == 'POST':
+			
+			# save edits  
+             sql = "INSERT INTO home_user(doctorname, doctornumber, doctorappointment) VALUES(%s, %s, %s)" 
+             data = (_doctorname, _doctornumber, _doctorappointment,)
+             conn = mysql.connect()
+             cursor = conn.cursor()
+             cursor.execute(sql, data)
+             conn.commit()
+             resp = jsonify('User added successfully!')
+             resp.status_code = 200
+             return resp
+        else:
+            return not_found()
+    except Exception as e:
+		     print(e)
+    finally:
+        cursor.close() 
+        conn.close()
+		
+@app.route('/appointments')
+def users():
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM doctor_app.appointment;")
+        rows = cursor.fetchall()
+        resp = jsonify(rows)
+        resp.status_code=200
+        print(rows)
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close() 
+        conn.close()
+		
+@app.route('/appointments/<int:id>')
+def user(id):
+	conn = None
+	cursor = None
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT user_id id,  user_doctorname doctorname, user_doctornumber doctornumber, user_doctorappointment doctorappointment FROM home_user WHERE user_id=%s", id)
+		row = cursor.fetchone()
+		resp = jsonify(row)
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/appointments/update', methods=['PUT'])
+def update_user():
+    conn = None
+    cursor = None
+    try:
+        _json = request.json
+        _doctorid = _json['doctorid']
+        _doctorname = _json['doctorname']
+        _doctornumber = _json['doctornumber']
+        _doctorappointment = _json['doctorappointment']		
+        # validate the received values
+        if _doctorname and _doctornumber and _doctorappointment and _doctorid and request.method == 'PUT':
+            print("test")
+            # save edits
+            sql = "UPDATE home_user SET doctorname=%s, doctornumber=%s, doctorappointment=%s WHERE  doctorid=%s"
+            data = (_doctorname, _doctornumber, _doctorappointment, _doctorid,)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sql, data)
+            conn.commit()
+            resp = jsonify('User updated successfully!')
+            resp.status_code = 200
+            return resp
+        else:
+            return not_found()
+    except Exception as e:
+        print(e)
+        print("test")
+    finally:
+        cursor.close() 
+        conn.close()
+		
+@app.route('/delete/<int:appointmentid>', methods=['DELETE'])
+def delete_user(appointmentid):
+	conn = None
+	cursor = None
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor()
+		cursor.execute("DELETE FROM appointment WHERE appointmentid=%s", (appointmentid,))
+		conn.commit()
+		resp = jsonify('User deleted successfully!')
+		resp.status_code = 200
+		return resp
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+		
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    
+    resp.status_code = 404
+
+    return resp
+		
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/heand.js
@@ -0,0 +1,130 @@
+// SHOW MENU
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            nav.classList.toggle('show')
+        });
+    }
+}
+
+showMenu('nav_toggle','nav_menu')
+
+// active & remove active
+const navlink = document.querySelectorAll('.nav_link')
+navlink.forEach(n => n.classList.remove('active'))
+
+function linkaction(){
+    navlink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    const navmenu = document.getElementById('nav_menu')
+    navmenu.classList.remove('show')
+}
+
+navlink.forEach(n => n.addEventListener('click', linkaction))
+fetch('/doctors')
+  .then(response => response.json())
+  .then(data => {
+
+            var doctors = '<tr>'
+            doctors += '<tread>'
+            doctors += '<th>name</th>'
+            doctors += '<th>degree</th>'
+            doctors += '<th>specification</th>'
+            doctors += '<th>startime</th>'
+            doctors += '<th>endtime</th>'
+            doctors += '</tread>'
+            doctors += '</tr>'
+
+            var doctors_names = '<option value=""> select doctorname</option>'
+            
+            
+        
+    data.forEach(function(value){
+        doctors += '<tr>'
+        doctors += '<tbody>'
+        doctors += '<td>'+value["doctorname"]+'</td>'
+        doctors += '<td>'+value["doctordegree"]+'</td>'
+        doctors += '<td>'+value["doctorspecification"]+'</td>'
+        doctors += '<td>'+value["doctorstarttime"]+'</td>'
+        doctors += '<td>'+value["doctorendtime"]+'</td>'
+        doctors += '</tbody>'
+        doctors += '</tr>'
+        document.getElementById("doctor_list").innerHTML = doctors
+       
+        doctors_names += '<option value="'+value["doctorname"]+'">'+value["doctorname"]+'</option>'
+        document.getElementById("doctors_names").innerHTML = doctors_names
+        console.log(value);
+       console.log(value["doctorname"]);
+    });
+    
+  });
+
+function fetch_appointments() {
+    fetch('/appointments')
+.then(response => response.json())
+.then(data =>{
+
+   var appointment = '<tr>'
+  appointment += '<tread>'
+  appointment += '<th>name</th>'
+  appointment += '<th>date</th>'
+  appointment += '<th>doctorname</th>'
+  appointment += '<th>email</th>'
+  appointment += '<th>text</th>'
+  appointment += '</tread>'
+  appointment += '</tr>'
+
+  data.forEach(function(value){
+      appointment += '<tr>'
+      appointment += '<tbody>'
+      appointment += '<td>'+value["appointmentname"]+'</td>'
+      appointment += '<td>'+value["appointmentdate"]+'</td>'
+      appointment += '<td>'+value["appointmentdoctorname"]+'</td>'
+      appointment += '<td>'+value["appointmentemail"]+'</td>'
+      appointment += '<td>'+value["appointmenttext"]+'</td>'
+      appointment += '</tbody>'
+      appointment += '</tr>'
+      document.getElementById("appointment_list").innerHTML = appointment
+
+  });
+});
+}
+
+fetch_appointments();
+
+  
+
+
+function create_appointment(form) {
+    console.log("test");
+    const formData = new URLSearchParams(new FormData(document.getElementById("appointment_form")));
+    console.log(formData);
+
+    var formobject = {};
+    formData.forEach(function(value, key){
+        formobject[key] = value;
+    });
+
+    formobject = JSON.stringify(formobject);
+
+    fetch("/create-appointment", {
+        method:"POST", 
+        body: formobject,
+        headers: {
+            "Content-Type": "application/json",
+            // "Content-Type": "multipart/form-data",
+        }
+      }).then(response => response.json())
+      .then(data => {
+        fetch_appointments();
+          console.log(data);
+      });
+}
+
+
+
